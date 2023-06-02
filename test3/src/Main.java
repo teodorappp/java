f3ed2af4c9372a330f2a69cc05fa8329dc680c0e
Popescu Teodora <tpopescuuu@gmail.com>
2023-06-02 06:17:40 +0000

    Main.java edited online with Bitbucket

--- test3/src/Main.java
+++ test3/src/Main.java
@@ -1,5 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       
+        Runnable runnable = () -> {
+            try {
+                
+                BufferedWriter writer = new BufferedWriter(new FileWriter("numere.txt", true));
+
+               
+                while (true) {
+                
+                    int numar = (int) (Math.random() * 991) + 10;
+
+                 
+                    writer.write(Integer.toString(numar));
+                    writer.newLine();
+                    writer.flush();
+
+                 
+                    Thread.sleep(10000);
+                }
+
+
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+       
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 }
